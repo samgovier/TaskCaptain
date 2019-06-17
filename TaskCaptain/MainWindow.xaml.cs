78f9dcd933479aa17e866ab18c6f2e7cdcfb63d1
samgovier <samgovier@gmail.com>
2019-06-16 19:11:47 -0500

    Build REST call to collect Todoist Data

--- TaskCaptain/MainWindow.xaml.cs
+++ TaskCaptain/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.VisualBasic;
 
 namespace TaskCaptain
 {
@@ -20,19 +23,46 @@ namespace TaskCaptain
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TodoistAcct _todoistAcct;
+        private HttpClient _todoistClient;
+        private string _todoistEndpoint = "https://beta.todoist.com/API/v8";
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTodoistAccount();
+            DataContext = _todoistAcct;
+        }
+
+        private void InitializeTodoistAccount()
+        {
+            string restApiToken = "4019f27f4a31859906535ff630dcac7ebb541062";
+            _todoistClient = new HttpClient();
+            _todoistClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + restApiToken);
+            string getProjectsString = _todoistClient.GetStringAsync(new Uri(_todoistEndpoint + "/projects")).Result;
+            string getTasksString = _todoistClient.GetStringAsync(new Uri(_todoistEndpoint + "/tasks")).Result;
+            List<TodoistProject> getProjectsList = JsonConvert.DeserializeObject<List<TodoistProject>>(getProjectsString);
+            List<TodoistTask> getTasksList = JsonConvert.DeserializeObject<List<TodoistTask>>(getTasksString);
+
+            //place each task in it's corresponding project
+            foreach(TodoistTask taskItem in getTasksList)
+            {
+                foreach(TodoistProject projectItem in getProjectsList)
+                {
+                    if(projectItem.Id == taskItem.ProjectId)
+                    {
+                        projectItem.Add(taskItem);
+                        break;
+                    }
+                }
+            }
+
+            _todoistAcct = new TodoistAcct(restApiToken, getProjectsList.ToArray());
         }
 
         private void SyncButton_Click(object sender, RoutedEventArgs e)
         {
 
-        }
-
-        private void login()
-        {
-            
         }
     }
 }
