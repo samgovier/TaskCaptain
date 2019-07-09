ff4963ba65831aa111b53700d1b3643bb8308588
samgovier <samgovier@gmail.com>
2019-07-09 05:39:18 -0500

    Move code to model/view folders

---
+++ TaskCaptain/Model/TodoistOfflineException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TaskCaptain
+{
+    internal class TodoistOfflineException : Exception
+    {
+        private string _message;
+
+        public override string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+
+        public TodoistOfflineException()
+        {
+            _message = "Todoist Project or Task is offline.";
+        }
+
+        public TodoistOfflineException(string message) : base(message)
+        {
+            _message = message;
+        }
+    }
+}
