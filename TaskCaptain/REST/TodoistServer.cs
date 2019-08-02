3cab3e71c25334af3b8782b943b8157b84d9653c
samgovier <samgovier@gmail.com>
2019-08-02 17:17:13 -0500

    Add Methods to TodoistServer

--- TaskCaptain/REST/TodoistServer.cs
+++ TaskCaptain/REST/TodoistServer.cs
@@ -12,5 +12,55 @@ namespace TaskCaptain.REST
         {
 
         }
+
+        public static CreateNewProject(string projectName)
+        {
+
+        }
+
+        public static GetProject(int projectId)
+        {
+
+        }
+
+        public static UpdateProject(string projectName)
+        {
+
+        }
+
+        public static DeleteProject(int projectId)
+        {
+
+        }
+
+        public static GetActiveTasks()
+        {
+
+        }
+
+        public static GetActiveTasks(int projectId)
+        {
+
+        }
+
+        public static GetActiveTasks(string filter)
+        {
+
+        }
+
+        public static GetActiveTasks(int projectId, string filter)
+        {
+
+        }
+
+        public static CreateNewTask(string content)
+        {
+
+        }
+
+        public static CreateNewTask(string content, int projectId, int order, int priority)
+        {
+
+        }
     }
 }
