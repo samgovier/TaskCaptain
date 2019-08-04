95f7537430d08a756dc3024a91ba8a246a71c015
samgovier <samgovier@gmail.com>
2019-08-04 18:27:59 -0500

    Update TodoistServer with HttpClient and Response Message

--- TaskCaptain/REST/TodoistServer.cs
+++ TaskCaptain/REST/TodoistServer.cs
@@ -3,64 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace TaskCaptain.REST
 {
     public static class TodoistServer
     {
-        //public static GetAllProjects()
-        //{
+        public static HttpResponseMessage GetAllProjects(HttpClient todoistClient, out ICollection<TodoistProject> allProjects)
+        {
 
-        //}
+        }
 
-        //public static CreateNewProject(string projectName)
-        //{
+        public static HttpResponseMessage CreateNewProject(HttpClient todoistClient, string projectName, out TodoistProject newProject)
+        {
 
-        //}
+        }
 
-        //public static GetProject(int projectId)
-        //{
+        public static HttpResponseMessage GetProject(HttpClient todoistClient, int projectId, out TodoistProject gotProject)
+        {
 
-        //}
+        }
 
-        //public static UpdateProject(string projectName)
-        //{
+        public static HttpResponseMessage UpdateProject(HttpClient todoistClient, string projectName)
+        {
 
-        //}
+        }
 
-        //public static DeleteProject(int projectId)
-        //{
+        public static HttpResponseMessage DeleteProject(HttpClient todoistClient, int projectId)
+        {
 
-        //}
+        }
 
-        //public static GetActiveTasks()
-        //{
+        public static HttpResponseMessage GetActiveTasks(HttpClient todoistClient, out ICollection<TodoistTask> allTasks)
+        {
 
-        //}
+        }
 
-        //public static GetActiveTasks(int projectId)
-        //{
+        public static HttpResponseMessage GetActiveTasks(HttpClient todoistClient, int projectId, out ICollection<TodoistTask> projectTasks)
+        {
 
-        //}
+        }
 
-        //public static GetActiveTasks(string filter)
-        //{
+        public static HttpResponseMessage GetActiveTasks(HttpClient todoistClient, string filter, out ICollection<TodoistTask> filterTasks)
+        {
 
-        //}
+        }
 
-        //public static GetActiveTasks(int projectId, string filter)
-        //{
+        public static HttpResponseMessage GetActiveTasks(HttpClient todoistClient, int projectId, string filter, out ICollection<TodoistTask> filterTasks)
+        {
 
-        //}
+        }
 
-        //public static CreateNewTask(string content)
-        //{
+        public static HttpResponseMessage CreateNewTask(HttpClient todoistClient, string content, out TodoistTask newTask)
+        {
 
-        //}
+        }
 
-        //public static CreateNewTask(string content, int projectId, int order, int priority)
-        //{
+        public static HttpResponseMessage CreateNewTask(HttpClient todoistClient, string content, int projectId, int order, int priority, TodoistDue due, out TodoistTask newTask)
+        {
 
-        //}
+        }
     }
 }
